89bb8d0d0ca20606455a847904b617a31e654a0d
Amrsaid208 <82062258+Amrsaid208@users.noreply.github.com>
2024-12-20 21:24:33 +0200

    [300] [Time Beats: 100.00%] [Memory Beats: 48.81%] - LeetPush

---
+++ 300-Longest-Increasing-Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> temp;
+        for(auto num:nums)
+        {
+            auto  it=lower_bound(temp.begin(),temp.end(),num);
+            if(it == temp.end())
+            {
+                temp.push_back(num);
+            }
+            else {
+                *it=num;
+            }
+        }
+        return temp.size();
+    }
+};
